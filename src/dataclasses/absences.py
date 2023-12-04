61dff51ffcd78300d34a79c84b5a188e7b597804
Nathan Tung <nelsan9789@gmail.com>
2023-12-04 04:00:46 +0000

    chore(absences.py): implement basic attendance types

--- src/dataclasses/absences.py
+++ src/dataclasses/absences.py
@@ -1,3 +1,16 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class AttendanceType(Enum):
+    ABSENCE = 1
+    PERSONAL = 2
+    SICK = 3
+    BEREAVEMENT = 4
+    PUBLIC = 5
+    LATE = 7
+
+
+@dataclass
 class Absences:
-    def __init__(self):
-        pass
+    lessons: [AttendanceType]
